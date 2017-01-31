456b09abd2b44830728a107ccd19e3b6dd512729
Beomseok Seo <predle@me.com>
2017-01-31 18:43:02 +0900

    Initial Commit

---
+++ DesignPatternObjc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DesignPatternObjc
+//
+//  Created by Beomseok Seo on 1/31/17.
+//  Copyright © 2017 Beomseok Seo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
